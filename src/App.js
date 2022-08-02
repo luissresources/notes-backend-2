9e42bcb1cac5f155946076fcbfcbae163d503d5e
Luiss Resources <luissresources@gmail.com>
2022-08-01 22:06:30 -0500

    README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Footer from './components/Footer'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import noteService from './services/notes'
 
-function App() {
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(response => {
+        setNotes(response)
+      })
+  }, [])
+
+
+  const onChangeNote = e => {
+    setNewNote(e.target.value)
+  }
+
+  const onSubmitAddNote = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+
+    noteService 
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote.data))
+      setShowAll(false)
+      setNewNote('')
+      noteService
+      .getAll()
+      .then(response => {
+        setNotes(response)
+      })
+    })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const filter = notes.filter(note => {
+    return (
+      showAll ?
+        note.important === showAll
+      :
+        note.important === true || note.important === false
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h1>Notes</h1>
+      <Notification 
+        message={errorMessage}
+      />
+      <div>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {!showAll ? 'important' : 'all' }
+          </button>
+        </div>      
+        <ul>
+          {
+            <>
+              {filter.map((note, i) => 
+                <Note
+                  key={i}
+                  note={note} 
+                  toggleImportance={() => toggleImportanceOf(note.id)}
+                />
+              )} 
+            </>
+          }
+        </ul>
+      </div>
+      <div>
+        <form  onSubmit={onSubmitAddNote}>
+          <input type="text" onChange={onChangeNote} value={newNote}/>
+          <button>Save</button>
+        </form>
+      </div>
+      <Footer />
+    </>
+  )
+} 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const notes = [{
+//     id: 1,
+//     note: 'HTML is easy',
+//     date: new Date().toISOString(),
+//     important: Math.random() < 0.5,
+//   },
+//   {
+//     id:2,
+//     note: 'Browser can execute only javascript',
+//     date: new Date().toISOString(),
+//     important: Math.random() < 0.5,
+//   },
+//   {
+//     id: 3,
+//     note: 'Most important methods of HTTPS-protocol are GET and POST',
+//     date: new Date().toISOString(),
+//     important: Math.random() < 0.5,
+//   },
+//   {
+//     id: 4,
+//     note: 'Page manipulation from console is easy',
+//     date: new Date().toISOString(),
+//     important: Math.random() < 0.5,
+//   }
+// ]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// axios
+//     .get('http://localhost:3001/notes')
+//     .then(response => {
+//       const notes = response.data
+//       console.log(notes);
+//     })
+
+ReactDOM.render(<App />, document.getElementById('root'))
